e3d41657b4e7f05a4e9f0a72cb2d4cfb707faae1
Grigoryan89 <gevor_grigoryan89@mail.ru>
2023-05-01 16:28:22 +0400

    Add all pages

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/welcome', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/contact', function () {
+    return view('contact');
+});
+Route::get('/', function () {
+    return view('index');
+});
+Route::get('/portfolio', function () {
+    return view('portfolio');
+});
+Route::get('/service', function () {
+    return view('service');
+});
